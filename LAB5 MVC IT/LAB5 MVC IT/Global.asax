112c8d85e5b49cd8817f5609a52d271c173f1581
Martin Bojmaliev <mbojmaliev@gmail.com>
2018-05-22 16:52:25 +0200

    Created new IT Lab5 mvc project

---
+++ LAB5 MVC IT/LAB5 MVC IT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LAB5_MVC_IT.MvcApplication" Language="C#" %>
---
+++ LAB5 MVC IT/LAB5 MVC IT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB5_MVC_IT.Startup))]
+namespace LAB5_MVC_IT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
