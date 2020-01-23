2699dee450173690c85983a4b9a08ce5c5688447
Martin Bojmaliev <mbojmaliev@gmail.com>
2020-01-23 18:14:05 +0100

    Add files via upload

---
+++ lab 5/LAB5 MVC IT/Models/AddRoleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB5_MVC_IT.Models
+{
+    public class AddRoleModel
+    {
+        public string SelectedRole { get; set; }
+        public List<string> Roles { get; set; }
+        public string Email { get; set; }
+    }
+}
